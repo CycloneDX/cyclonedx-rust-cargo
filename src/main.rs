b3c74598fdc5bde7518c240382b7e700e1bc977b
Ted Driggs <ted.driggs@outlook.com>
2021-05-13 06:53:25 -0700

    Break up main method into separate concerns
    * Add Component struct to represent `<component>` BOM elements. This
      will enable adding a `Serialize` impl in a future commit to handle
      JSON serialization for CycloneDX 1.2 support.
    * Add ToXml trait for handling XML formatting while keeping exact
      control over the emitted XML.
    * Remove dead license extraction code.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 /**
 * A special acknowledgement Ossi Herrala from SensorFu for providing a
 * starting point in which to develop this plugin. The original project
@@ -44,7 +43,6 @@
 * SOFTWARE.
 */
 use std::collections::BTreeSet;
-use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::LineWriter;
@@ -57,11 +55,15 @@ use cargo::core::{Package, Resolve, Workspace};
 use cargo::ops;
 use cargo::util::Config;
 use cargo::CargoResult;
-use packageurl::PackageUrl;
-use regex::Regex;
 use structopt::StructOpt;
 use uuid::Uuid;
 use xml_writer::XmlWriter;
+
+mod component;
+mod traits;
+
+use component::Component;
+use traits::ToXml;
 
 #[derive(StructOpt)]
 #[structopt(bin_name = "cargo")]
@@ -140,134 +142,27 @@ fn real_main(config: &mut Config, args: Args) -> Result<(), Error> {
         top_level_dependencies(&members, package_ids)?
     };
 
-    //let p = &Package::new();
-
-
     let file = File::create("bom.xml")?;
-    let mut file = LineWriter::new(file);
+    let file = LineWriter::new(file);
     let mut xml = XmlWriter::new(file);
-    xml.dtd("UTF-8");
-    xml.begin_elem("bom");
-    xml.attr("serialNumber", Uuid::new_v4().to_urn().to_string().as_mut_str());
-    xml.attr("version", "1");
-    xml.attr("xmlns", "http://cyclonedx.org/schema/bom/1.1");
-    xml.begin_elem("components");
+    xml.dtd("UTF-8")?;
+    xml.begin_elem("bom")?;
+    xml.attr("serialNumber", &Uuid::new_v4().to_urn().to_string())?;
+    xml.attr("version", "1")?;
+    xml.attr("xmlns", "http://cyclonedx.org/schema/bom/1.1")?;
 
+    xml.begin_elem("components")?;
     for package in &dependencies {
-        let name = package.name().to_owned().as_str().trim();
-        let version = format!("{}", package.version());
-        xml.begin_elem("component");
-        xml.attr("type", "library");
-
-        xml.begin_elem("name");
-        xml.text(name);
-        xml.end_elem();
-
-        xml.begin_elem("version");
-        xml.text(version.as_str().trim());
-        xml.end_elem();
-
-        match &package.manifest().metadata().description {
-            Some(x) => {
-                xml.begin_elem("description");
-                xml.cdata(x.trim());
-                xml.end_elem();
-            },
-            None => { }
-        }
-
-        xml.begin_elem("scope");
-        xml.text("required");
-        xml.end_elem();
-
-        //TODO: Add hashes. May require file components and manual calculation of all files
-
-        //let licenses = format!("{}", package_licenses(package));
-        //println!("{}", licenses);
-        if package.manifest().metadata().license.is_some() {
-            match &package.manifest().metadata().license {
-                Some(x) => {
-                    xml.begin_elem("licenses");
-                    xml.begin_elem("license");
-                    xml.begin_elem("expression");
-                    xml.text(x.trim());
-                    xml.end_elem();
-                    xml.end_elem();
-                    xml.end_elem();
-                },
-                None => { }
-            }
-        }
-
-        let mut purl = PackageUrl::new("cargo", name).with_version(version.as_str().trim()).to_string();
-        xml.begin_elem("purl");
-        xml.text(purl.as_mut_str());
-        xml.end_elem();
-
-        if package.manifest().metadata().documentation.is_some()
-            | package.manifest().metadata().homepage.is_some()
-            | package.manifest().metadata().links.is_some()
-            | package.manifest().metadata().repository.is_some() {
-
-            let re = Regex::new(r"^([a-z0-9+.-]+):(?://(?:((?:[a-z0-9-._~!$&'()*+,;=:]|%[0-9A-F]{2})*)@)?((?:[a-z0-9-._~!$&'()*+,;=]|%[0-9A-F]{2})*)(?::(\d*))?(/(?:[a-z0-9-._~!$&'()*+,;=:@/]|%[0-9A-F]{2})*)?|(/?(?:[a-z0-9-._~!$&'()*+,;=:@]|%[0-9A-F]{2})+(?:[a-z0-9-._~!$&'()*+,;=:@/]|%[0-9A-F]{2})*)?)(?:\?((?:[a-z0-9-._~!$&'()*+,;=:/?@]|%[0-9A-F]{2})*))?(?:#((?:[a-z0-9-._~!$&'()*+,;=:/?@]|%[0-9A-F]{2})*))?$").unwrap();
-            xml.begin_elem("externalReferences");
-            match &package.manifest().metadata().documentation {
-                Some(x) => {
-                    if re.is_match(x) {
-                        xml.begin_elem("reference");
-                        xml.attr("type", "documentation");
-                        xml.text(x.trim());
-                        xml.end_elem();
-                    }
-                },
-                None => { }
-            }
-            match &package.manifest().metadata().homepage {
-                Some(x) => {
-                    if re.is_match(x) {
-                        xml.begin_elem("reference");
-                        xml.attr("type", "website");
-                        xml.text(x.trim());
-                        xml.end_elem();
-                    }
-                },
-                None => { }
-            }
-            match &package.manifest().metadata().links {
-                Some(x) => {
-                    if re.is_match(x) {
-                        xml.begin_elem("reference");
-                        xml.attr("type", "other");
-                        xml.text(x.trim());
-                        xml.end_elem();
-                    }
-                },
-                None => { }
-            }
-            match &package.manifest().metadata().repository {
-                Some(x) => {
-                    if re.is_match(x) {
-                        xml.begin_elem("reference");
-                        xml.attr("type", "vcs");
-                        xml.text(x.trim());
-                        xml.end_elem();
-                    }
-                },
-                None => { }
-            }
-            xml.end_elem();
-        }
-
-
-        xml.end_elem(); // end component
+        Component::from(package).to_xml(&mut xml)?;
     }
+    xml.end_elem()?; // end components
 
-    xml.end_elem(); // end components
     // TODO: Add dependency graph
-    xml.end_elem(); // end bom
-    xml.close();
-    xml.flush();
-    let actual = xml.into_inner();
+
+    xml.end_elem()?; // end bom
+    xml.close()?;
+    xml.flush()?;
+    let _actual = xml.into_inner();
 
     Ok(())
 }
@@ -320,68 +215,6 @@ fn all_dependencies(
     }
 
     Ok(dependencies)
-}
-
-fn package_licenses(package: &Package) -> Licenses<'_> {
-    let metadata = package.manifest().metadata();
-
-    if let Some(ref license_str) = metadata.license {
-        let licenses: BTreeSet<&str> = license_str
-            .split("OR")
-            .map(|s| s.split("AND"))
-            .flatten()
-            .map(|s| s.split('/'))
-            .flatten()
-            .map(str::trim)
-            .collect();
-        return Licenses::Licenses(licenses);
-    }
-
-    if let Some(ref license_file) = metadata.license_file {
-        return Licenses::File(license_file);
-    }
-
-    Licenses::Missing
-}
-
-static LICENCE_FILE_NAMES: &[&str] = &["LICENSE", "UNLICENSE"];
-
-fn package_license_files(package: &Package) -> io::Result<Vec<path::PathBuf>> {
-    let mut result = Vec::new();
-    if let Some(path) = package.manifest_path().parent() {
-        for entry in path.read_dir()? {
-            if let Ok(entry) = entry {
-                if let Ok(name) = entry.file_name().into_string() {
-                    for license_name in LICENCE_FILE_NAMES {
-                        if name.starts_with(license_name) {
-                            result.push(entry.path())
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Ok(result)
-}
-
-#[derive(Debug)]
-enum Licenses<'a> {
-    Licenses(BTreeSet<&'a str>),
-    File(&'a str),
-    Missing,
-}
-
-impl<'a> fmt::Display for Licenses<'a> {
-    fn fmt(self: &Self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        match *self {
-            Licenses::File(_) => write!(f, "Specified in license file"),
-            Licenses::Missing => write!(f, "Missing"),
-            Licenses::Licenses(ref lic_names) => {
-                let lics: Vec<String> = lic_names.iter().map(|s| String::from(*s)).collect();
-                write!(f, "{}", lics.join(", "))
-            }
-        }
-    }
 }
 
 #[derive(Debug)]
