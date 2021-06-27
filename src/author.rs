59e8378b2b9b5e92733cc2a55b4974f8fbb12072
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-06-26 23:40:55 -0800

    Better?

--- src/author.rs
+++ src/author.rs
@@ -1,48 +1,28 @@
+/*
+ * This file is part of CycloneDX Rust Cargo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 use std::{io, str::FromStr};
 
-use cargo::core::Package;
 use lazy_static::lazy_static;
 use regex::Regex;
 use serde::Serialize;
 use xml_writer::XmlWriter;
 
-use crate::traits::{IsEmpty, ToXml};
-
-#[derive(Serialize)]
-pub struct Authors(Vec<Author>);
-
-impl<'a> From<&'a Package> for Authors {
-    fn from(pkg: &'a Package) -> Self {
-        Self(
-            pkg.manifest()
-                .metadata()
-                .authors
-                .iter()
-                .filter_map(|author| Author::from_str(author).ok())
-                .collect(),
-        )
-    }
-}
-
-impl IsEmpty for Authors {
-    fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-}
-
-impl ToXml for Authors {
-    fn to_xml<W: io::Write>(&self, xml: &mut XmlWriter<W>) -> io::Result<()> {
-        if !self.is_empty() {
-            xml.begin_elem("authors")?;
-            for author in &self.0 {
-                author.to_xml(xml)?;
-            }
-            xml.end_elem()?;
-        }
-
-        Ok(())
-    }
-}
+use crate::traits::{ToXml};
 
 lazy_static! {
     static ref EMAIL_REGEX: Regex = Regex::new(r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#).unwrap();
@@ -71,6 +51,18 @@ impl Author {
         }
 
         Ok(Self { name, email })
+    }
+}
+
+impl ToXml for Vec<Author> {
+    fn to_xml<W: io::Write>(&self, xml: &mut XmlWriter<W>) -> io::Result<()> {
+        xml.begin_elem("authors")?;
+
+        for author in self {
+            author.to_xml(xml)?;
+        }
+
+        xml.end_elem()
     }
 }
 
--- src/bom/metadata.rs
+++ src/bom/metadata.rs
@@ -1,17 +1,36 @@
+/*
+ * This file is part of CycloneDX Rust Cargo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+use xml_writer::XmlWriter;
 use std::io;
 
 use cargo::core::{Package, TargetKind};
 use chrono::{DateTime, Utc};
 use serde::Serialize;
-use xml_writer::XmlWriter;
 
-use crate::{author::Authors, Component, IsEmpty, ToXml};
+use crate::traits::{ToXml};
+use crate::component::Component;
+use crate::author::Author;
 
 #[derive(Serialize)]
 pub struct Metadata<'a> {
-    pub timestamp: DateTime<Utc>,
-    #[serde(skip_serializing_if = "IsEmpty::is_empty")]
-    pub authors: Option<Authors>,
+    pub timestamp: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub authors: Option<Vec<Author>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub component: Option<Component<'a>>,
 }
@@ -19,9 +38,9 @@ pub struct Metadata<'a> {
 impl<'a> Default for Metadata<'a> {
     fn default() -> Self {
         Self {
-            timestamp: Utc::now(),
-            authors: None,
-            component: None,
+            timestamp: Some(Utc::now()),
+            authors: None::<Vec<Author>>,
+            component: None::<Component>
         }
     }
 }
@@ -29,7 +48,7 @@ impl<'a> Default for Metadata<'a> {
 impl<'a> From<&'a Package> for Metadata<'a> {
     fn from(pkg: &'a Package) -> Self {
         Self {
-            authors: Some(Authors::from(pkg)),
+            // authors: Some(Authors::from(pkg)),
             component: Some(if could_be_application(pkg) {
                 Component::application(pkg).without_scope()
             } else {
@@ -44,12 +63,13 @@ impl ToXml for Metadata<'_> {
     fn to_xml<W: io::Write>(&self, xml: &mut XmlWriter<W>) -> io::Result<()> {
         xml.begin_elem("metadata")?;
 
-        xml.elem_text(
-            "timestamp",
-            &self
-                .timestamp
-                .to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
-        )?;
+        if let Some(timestamp) = &self.timestamp {
+            xml.elem_text(
+                "timestamp",
+                    &timestamp
+                    .to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            )?;
+        }
 
         if let Some(authors) = &self.authors {
             authors.to_xml(xml)?;
