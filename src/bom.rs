4f93d1ed2cb46ab6e5483c7441f2f8b2e1b1c20c
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-08-25 14:37:28 -0800

    Fix up metadata, maybe? (#28)
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Co-authored-by: Steve Springett <steve@springett.us>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Deploy workflow (#9)
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Ok
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Remove check on event
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Come on
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Maybe
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Down to just grep
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Ok
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Try and set the variable now
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Commmmme on
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    WAT
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    What the hell github
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    MAD
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Prefix maybe
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Use set output
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Drop space
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    switch to manual workflow
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Echo it
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Switch access method
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Ok now push
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Ok now push the tag
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    New development bump to 0.1.2
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Publish to crates before git
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    New development bump to 0.1.3
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Publish
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    New development bump to 0.1.4
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Verbose and allow to publish without changes committed (that will occur later)
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Build
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    New development bump to 0.1.5
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Lock update
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    New development bump to 0.1.6
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
    
    Swap back to 0.1.1
    
    Signed-off-by: Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>

--- src/bom.rs
+++ src/bom.rs
@@ -16,13 +16,15 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright (c) OWASP Foundation. All Rights Reserved.
  */
-use std::{io, iter::FromIterator};
+use std::io;
+use std::iter::FromIterator;
 
 use cargo::core::Package;
 use serde::{Serialize, Serializer};
 use uuid::Uuid;
 use xml_writer::XmlWriter;
 
+use crate::metadata::Metadata;
 use crate::{Component, ToXml};
 
 static SPEC_VERSION: &'static str = "1.3";
@@ -39,29 +41,42 @@ fn uuid_to_urn<S: Serializer>(uuid: &Uuid, serializer: S) -> Result<S::Ok, S::Er
 /// A software bill of materials for a Rust crate.
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Bom<'a> {
+pub struct Bom {
     bom_format: BomFormat,
     spec_version: &'static str,
     #[serde(serialize_with = "uuid_to_urn")]
     serial_number: Uuid,
     version: u32,
-    components: Vec<Component<'a>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub metadata: Option<Metadata>,
+    components: Vec<Component>,
 }
 
-/// Create a new BOM from a sequence of cargo package references.
-impl<'a> FromIterator<&'a Package> for Bom<'a> {
-    fn from_iter<T: IntoIterator<Item = &'a Package>>(iter: T) -> Self {
+impl<'a> Default for Bom {
+    fn default() -> Bom {
         Self {
             bom_format: BomFormat::CycloneDX,
             spec_version: SPEC_VERSION,
             version: 1,
             serial_number: Uuid::new_v4(),
-            components: iter.into_iter().map(Component::library).collect(),
+            metadata: None::<Metadata>,
+            components: Vec::new(),
         }
     }
 }
 
-impl ToXml for Bom<'_> {
+/// Create a new BOM from a sequence of cargo package references.
+impl<'a> FromIterator<&'a Package> for Bom {
+    fn from_iter<T: IntoIterator<Item = &'a Package>>(iter: T) -> Self {
+        Self {
+            components: iter.into_iter().map(Component::from).collect(),
+            metadata: Some(Metadata::default()),
+            ..Default::default()
+        }
+    }
+}
+
+impl ToXml for Bom {
     fn to_xml<W: io::Write>(&self, xml: &mut XmlWriter<W>) -> io::Result<()> {
         let namespace = format!("http://cyclonedx.org/schema/bom/{}", SPEC_VERSION);
         xml.dtd("UTF-8")?;
@@ -69,6 +84,10 @@ impl ToXml for Bom<'_> {
         xml.attr("serialNumber", &self.serial_number.to_urn().to_string())?;
         xml.attr("version", "1")?;
         xml.attr("xmlns", namespace.as_str())?;
+
+        if let Some(metadata) = &self.metadata {
+            metadata.to_xml(xml)?;
+        }
 
         xml.begin_elem("components")?;
         for component in &self.components {
---
+++ src/metadata.rs
@@ -0,0 +1,161 @@
+/*
+ * This file is part of CycloneDX Rust Cargo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
+ */
+use std::io;
+use xml_writer::XmlWriter;
+
+use cargo::core::{Package, TargetKind};
+use chrono::{DateTime, Utc};
+use log::debug;
+use serde::Serialize;
+use std::str::FromStr;
+
+use crate::author::Author;
+use crate::component::Component;
+use crate::traits::ToXml;
+
+#[derive(Serialize)]
+pub struct Metadata {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timestamp: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub authors: Option<Vec<Author>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub component: Option<Component>,
+}
+
+impl<'a> Default for Metadata {
+    fn default() -> Self {
+        Self {
+            timestamp: Some(Utc::now()),
+            authors: None::<Vec<Author>>,
+            component: None::<Component>,
+        }
+    }
+}
+
+impl<'a> From<&'a Package> for Metadata {
+    fn from(pkg: &'a Package) -> Self {
+        Self {
+            authors: get_authors_from_package(pkg),
+            component: Some(if could_be_application(pkg) {
+                Component::application(pkg).without_scope()
+            } else {
+                Component::library(pkg).without_scope()
+            }),
+            ..Default::default()
+        }
+    }
+}
+
+impl<'a> From<&'a cargo_metadata::Package> for Metadata {
+    fn from(package: &'a cargo_metadata::Package) -> Self {
+        Self {
+            authors: get_authors_from_package_cm(package),
+            component: Some(if is_an_application(package) {
+                Component::application_cm(package).without_scope()
+            } else {
+                Component::library_cm(package).without_scope()
+            }),
+            ..Default::default()
+        }
+    }
+}
+
+impl ToXml for Metadata {
+    fn to_xml<W: io::Write>(&self, xml: &mut XmlWriter<W>) -> io::Result<()> {
+        xml.begin_elem("metadata")?;
+
+        if let Some(timestamp) = &self.timestamp {
+            xml.elem_text(
+                "timestamp",
+                &timestamp.to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            )?;
+        }
+
+        if let Some(authors) = &self.authors {
+            authors.to_xml(xml)?;
+        }
+
+        if let Some(component) = &self.component {
+            component.to_xml(xml)?;
+        }
+
+        xml.end_elem()
+    }
+}
+
+fn get_authors_from_package<'a>(package: &'a Package) -> Option<Vec<Author>> {
+    let mut authors = Vec::new();
+    let mut invalid_authors = Vec::new();
+
+    for author in package.authors() {
+        match Author::from_str(&author) {
+            Ok(author) => authors.push(author),
+            Err(e) => invalid_authors.push((author, e)),
+        }
+    }
+    invalid_authors
+        .into_iter()
+        .for_each(|(author, error)| debug!("Invalid author {}: {:?}", author, error));
+
+    if authors.len() > 0 {
+        return Some(authors);
+    }
+
+    None
+}
+
+fn get_authors_from_package_cm<'a>(package: &'a cargo_metadata::Package) -> Option<Vec<Author>> {
+    let mut authors = Vec::new();
+    let mut invalid_authors = Vec::new();
+
+    for author in &package.authors {
+        match Author::from_str(&author) {
+            Ok(author) => authors.push(author),
+            Err(e) => invalid_authors.push((author, e)),
+        }
+    }
+    invalid_authors
+        .into_iter()
+        .for_each(|(author, error)| debug!("Invalid author {}: {:?}", author, error));
+
+    if authors.len() > 0 {
+        return Some(authors);
+    }
+
+    None
+}
+
+/// Check if `pkg` might be an executable application based on the presence of binary targets.
+fn could_be_application(pkg: &Package) -> bool {
+    pkg.targets()
+        .iter()
+        .any(|tgt| *tgt.kind() == TargetKind::Bin)
+}
+
+/// Is the `package` an application? This will tell us!
+fn is_an_application(pkg: &cargo_metadata::Package) -> bool {
+    let mut kinds = pkg
+        .targets
+        .iter()
+        .map(|target| target.clone().kind)
+        .flatten();
+
+    kinds.any(|kind| kind == "bin")
+}
