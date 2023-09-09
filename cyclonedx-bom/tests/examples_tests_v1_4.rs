6fdddfbbd8f88d52c1d58f1087ae84f9608731a8
tokcum <tobias.mucke@gmail.com>
2023-09-09 17:05:00 +0200

    upd: 1.4 spec
    Signed-off-by: tokcum <tobias.mucke@gmail.com>

--- cyclonedx-bom/tests/examples_tests_v1_4.rs
+++ cyclonedx-bom/tests/examples_tests_v1_4.rs
@@ -5,30 +5,30 @@ mod examples {
     #[test]
     fn it_should_parse_all_of_the_valid_json_examples() {
         insta::with_settings!({
-        snapshot_path => "examples/snapshots/1.4",
-        prepend_module_to_snapshot => false,
-    }, {
-        insta::glob!("examples/1.4/valid*.cdx.json", |path| {
-            let file = std::fs::File::open(path).expect(&format!("Failed to read file: {path:?}"));
-            let bom = Bom::parse_from_json_v1_4(file).expect(&format!(
-                "Failed to parse the document as an SBOM: {path:?}"
-            ));
+            snapshot_path => "examples/snapshots/1.4",
+            prepend_module_to_snapshot => false,
+        }, {
+            insta::glob!("examples/1.4/valid*.cdx.json", |path| {
+                let file = std::fs::File::open(path).expect(&format!("Failed to read file: {path:?}"));
+                let bom = Bom::parse_from_json_v1_4(file).expect(&format!(
+                    "Failed to parse the document as an SBOM: {path:?}"
+                ));
 
-            let validation_result = bom.validate().expect("Failed to validate BOM");
-            assert_eq!(
-                validation_result,
-                ValidationResult::Passed,
-                "{path:?} unexpectedly failed validation"
-            );
+                let validation_result = bom.validate().expect("Failed to validate BOM");
+                assert_eq!(
+                    validation_result,
+                    ValidationResult::Passed,
+                    "{path:?} unexpectedly failed validation"
+                );
 
-            let mut output = Vec::new();
-            bom.output_as_json_v1_4(&mut output)
-                .expect(&format!("Failed to output the file: {path:?}"));
-            let bom_output = String::from_utf8_lossy(&output).to_string();
+                let mut output = Vec::new();
+                bom.output_as_json_v1_4(&mut output)
+                    .expect(&format!("Failed to output the file: {path:?}"));
+                let bom_output = String::from_utf8_lossy(&output).to_string();
 
-            insta::assert_snapshot!(bom_output);
+                insta::assert_snapshot!(bom_output);
+            });
         });
-    });
     }
 
     #[test]
