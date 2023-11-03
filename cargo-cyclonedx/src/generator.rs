dcc269ef4e4623f05a244d4f0044ca9760778f2b
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-11-03 01:01:21 +0000

    Add empty lines for readability
    Signed-off-by: Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>

--- cargo-cyclonedx/src/generator.rs
+++ cargo-cyclonedx/src/generator.rs
@@ -188,6 +188,7 @@ impl SbomGenerator {
                     log::error!("Target {} is neither a binary nor a library!", tgt.name);
                     continue;
                 };
+
                 // bom_ref
                 let bom_ref = format!(
                     "{} bin-target-{}",
@@ -195,6 +196,7 @@ impl SbomGenerator {
                     subcomp_count
                 );
                 subcomp_count += 1;
+
                 // put it all together
                 subcomponents.push(Component::new(
                     cdx_type,
