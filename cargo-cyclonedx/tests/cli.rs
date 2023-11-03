aa0673623e54c94572f9dd4477c637335a54a014
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-11-03 02:12:37 +0000

    Update test to look for the new filename
    Signed-off-by: Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>

--- cargo-cyclonedx/tests/cli.rs
+++ cargo-cyclonedx/tests/cli.rs
@@ -146,6 +146,7 @@ fn bom_file_name_extension_is_prepended_with_cdx() -> Result<(), Box<dyn std::er
 
     cmd.current_dir(tmp_dir.path())
         .arg("cyclonedx")
+        .arg("--no-target-suffix")
         .arg("--output-cdx");
 
     cmd.assert().success().stdout("");
