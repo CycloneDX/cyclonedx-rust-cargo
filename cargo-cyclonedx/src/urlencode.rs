1c669ea36cb06cd49eff73407f414ae64437f33b
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-11-12 17:41:46 +0000

    Comment: link to a later comment in the issue with more details
    Signed-off-by: Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>

--- cargo-cyclonedx/src/urlencode.rs
+++ cargo-cyclonedx/src/urlencode.rs
@@ -111,7 +111,7 @@ use percent_encoding::{self, utf8_percent_encode, AsciiSet, NON_ALPHANUMERIC};
 /// We use the _character set_ but not the _encoding scheme_ of form-urlencoded.
 /// The difference is that on top of the character set it also defines that ' ' (space) should be encoded as '+', not "%20".
 /// However, official decoder implementations are split about 50/50 on whether they decode '+' as '+' or as a space:
-/// <https://github.com/phylum-dev/purl/issues/11#issuecomment-1791544712>
+/// <https://github.com/phylum-dev/purl/issues/11#issuecomment-1799192916>
 /// Therefore, we never emit the '+' sign to avoid issues with half of the extant implementations.
 const FORM_URLENCODED: &AsciiSet = &NON_ALPHANUMERIC
     .remove(b'*')
