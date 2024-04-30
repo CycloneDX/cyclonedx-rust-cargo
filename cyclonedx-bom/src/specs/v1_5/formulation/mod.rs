54a485675af41393c4e7207d8ed6d63983d08c69
Christian Poveda <christian.poveda@ferrous-systems.com>
2024-04-30 16:34:53 -0500

    add dummy `Formulation` spec
    Signed-off-by: Christian Poveda <christian.poveda@ferrous-systems.com>

--- cyclonedx-bom/src/specs/v1_5/formulation/mod.rs
+++ cyclonedx-bom/src/specs/v1_5/formulation/mod.rs
@@ -1,2 +1,16 @@
 mod runtime_topology;
 mod workflow;
+
+// #definitions/formula
+pub(crate) struct Formulation {
+    // TODO(@divma): adjust type
+    bom_ref: u8,
+    // TODO(@divma): adjust type
+    components: u8,
+    // TODO(@divma): adjust type
+    services: u8,
+    // TODO(@divma): adjust type
+    workflows: u8,
+    // TODO(@divma): adjust type
+    properties: u8,
+}
