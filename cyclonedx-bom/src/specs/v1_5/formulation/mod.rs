8d74e8c3a24e7ad97aea993773eefd6365f6b8b5
Christian Poveda <christian.poveda@ferrous-systems.com>
2024-04-30 18:10:08 -0500

    set (potentially) appropiate types for `bom_ref`, `components`, `services`, `properties`
    Signed-off-by: Christian Poveda <christian.poveda@ferrous-systems.com>

--- cyclonedx-bom/src/specs/v1_5/formulation/mod.rs
+++ cyclonedx-bom/src/specs/v1_5/formulation/mod.rs
@@ -1,15 +1,15 @@
+use crate::specs::common::{self, property::Properties};
+
+use super::{component::Components, service::Services};
+
 mod runtime_topology;
 mod workflow;
 
 // #definitions/formula
 pub(crate) struct Formulation {
-    bom_ref: crate::specs::common::bom_reference::BomReference,
-    // TODO(@divma): adjust type
-    components: u8,
-    // TODO(@divma): adjust type
-    services: u8,
-    // TODO(@divma): adjust type
+    bom_ref: Option<common::bom_reference::BomReference>,
+    components: Option<Components>,
+    services: Option<Services>,
     workflows: u8,
-    // TODO(@divma): adjust type
-    properties: u8,
+    properties: Option<Properties>,
 }
