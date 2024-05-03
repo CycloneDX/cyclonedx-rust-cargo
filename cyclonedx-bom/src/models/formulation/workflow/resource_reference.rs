9a19d4a0136c26ee14c181274aa6f6df5cacb26c
Christian Poveda <christian.poveda@ferrous-systems.com>
2024-05-03 16:22:53 -0500

    Validate `ResourceReference`
    Signed-off-by: Christian Poveda <christian.poveda@ferrous-systems.com>

--- cyclonedx-bom/src/models/formulation/workflow/resource_reference.rs
+++ cyclonedx-bom/src/models/formulation/workflow/resource_reference.rs
@@ -1,9 +1,23 @@
-use crate::models::{bom::BomReference, external_reference::{BomLink, ExternalReference}};
+use crate::{
+    models::external_reference::ExternalReference, prelude::Validate, validation::ValidationContext,
+};
 
 pub(crate) enum ResourceReference {
-    Ref {
-        r#ref: Option<BomReference>,
-        bom_link: Option<BomLink>,
-    },
+    Ref(String),
     ExternalReference(ExternalReference),
+}
+
+impl Validate for ResourceReference {
+    fn validate_version(
+        &self,
+        version: crate::prelude::SpecVersion,
+    ) -> crate::prelude::ValidationResult {
+        let mut ctx = ValidationContext::new();
+
+        if let ResourceReference::ExternalReference(external_reference) = self {
+            ctx.add_struct("external_reference", external_reference, version);
+        }
+
+        ctx.into()
+    }
 }
