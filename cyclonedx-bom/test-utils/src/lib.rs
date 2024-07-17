edee7b6cd66e1c526af01e7a6c3b54345cf730f4
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-07-17 05:24:06 +0100

    cargo fmt
    Signed-off-by: Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>

--- cyclonedx-bom/test-utils/src/lib.rs
+++ cyclonedx-bom/test-utils/src/lib.rs
@@ -16,112 +16,112 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
- use jsonschema::{error::ValidationErrorKind, paths::JSONPointer, JSONSchema};
+use jsonschema::{error::ValidationErrorKind, paths::JSONPointer, JSONSchema};
 
- use cyclonedx_bom::models::bom::SpecVersion;
- 
- #[derive(Debug)]
- pub struct ValidationError {
-     pub instance: String,
-     pub kind: ValidationErrorKind,
-     pub hierarchy: Vec<String>,
- }
- 
- impl ValidationError {
-     pub fn new(instance: String, kind: ValidationErrorKind, hierarchy: JSONPointer) -> Self {
-         let hierarchy = hierarchy
-             .iter()
-             .map(|chunk| format!("{:?}", chunk))
-             .collect::<Vec<_>>();
-         dbg!(&hierarchy);
- 
-         Self {
-             instance,
-             kind,
-             hierarchy,
-         }
-     }
- }
- 
- /// Validates a [`serde_json::Value`] against a JSON schema defined by the given [`SpecVersion`].
- ///
- /// This function returns the list of validation errors if something fails.
- ///
- /// ## Example
- ///
- /// ```rust
- /// use cyclonedx_bom::prelude::*;
- /// use test_utils::validate_json_with_schema;
- ///
- /// let bom_json = r#"{
- ///   "bomFormat": "CycloneDX",
- ///   "specVersion": "1.3",
- ///   "serialNumber": "urn:uuid:3e671687-395b-41f5-a30f-a58921a69b79",
- ///   "version": 1
- /// }"#;
- /// let json: serde_json::Value = serde_json::from_str(bom_json).expect("Failed to parse JSON");
- /// let result = validate_json_with_schema(&json, SpecVersion::V1_3);
- /// assert!(result.is_ok());
- /// ```
- ///
- pub fn validate_json_with_schema(
-     json: &serde_json::Value,
-     version: SpecVersion,
- ) -> Result<(), Vec<ValidationError>> {
-     let spdx_schema = include_str!("../schema/spdx.schema.json");
-     let spdx_schema: serde_json::Value =
-         serde_json::from_str(spdx_schema).expect("Failed to read spdx.schema.json");
-     let jsf_schema = include_str!("../schema/jsf-0.82.schema.json");
-     let jsf_schema: serde_json::Value =
-         serde_json::from_str(jsf_schema).expect("Failed to load jsf-0.82.schema.json");
- 
-     let schema = match version {
-         SpecVersion::V1_3 => include_str!("../schema/bom-1.3.schema.json"),
-         SpecVersion::V1_4 => include_str!("../schema/bom-1.4.schema.json"),
-         SpecVersion::V1_5 => include_str!("../schema/bom-1.5.schema.json"),
-     };
-     let schema: serde_json::Value =
-         serde_json::from_str(schema).expect("Failed to parse JSON schema file");
- 
-     // Fill in external schema files, handle unknown format(s)
-     let compiled_schema = JSONSchema::options()
-         .with_draft(jsonschema::Draft::Draft7)
-         .with_document(
-             "http://cyclonedx.org/schema/spdx.schema.json".to_string(),
-             spdx_schema,
-         )
-         .with_document(
-             "http://cyclonedx.org/schema/jsf-0.82.schema.json".to_string(),
-             jsf_schema,
-         )
-         .with_format("idn-email", with_idn_email)
-         .compile(&schema)
-         .expect("Failed to compile JSON schema file");
- 
-     let result = compiled_schema.validate(json);
-     if let Err(errors) = result {
-         let errors = errors.collect::<Vec<_>>();
-         dbg!(&errors);
-     }
-     compiled_schema.validate(json).map_err(|iter| {
-         iter.map(|err| ValidationError::new(err.instance.to_string(), err.kind, err.instance_path))
-             .collect::<Vec<_>>()
-     })
- }
- 
- /// For now ignore the content of the given email string.
- fn with_idn_email(_s: &str) -> bool {
-     true
- }
- 
- #[cfg(test)]
- mod test {
+use cyclonedx_bom::models::bom::SpecVersion;
+
+#[derive(Debug)]
+pub struct ValidationError {
+    pub instance: String,
+    pub kind: ValidationErrorKind,
+    pub hierarchy: Vec<String>,
+}
+
+impl ValidationError {
+    pub fn new(instance: String, kind: ValidationErrorKind, hierarchy: JSONPointer) -> Self {
+        let hierarchy = hierarchy
+            .iter()
+            .map(|chunk| format!("{:?}", chunk))
+            .collect::<Vec<_>>();
+        dbg!(&hierarchy);
+
+        Self {
+            instance,
+            kind,
+            hierarchy,
+        }
+    }
+}
+
+/// Validates a [`serde_json::Value`] against a JSON schema defined by the given [`SpecVersion`].
+///
+/// This function returns the list of validation errors if something fails.
+///
+/// ## Example
+///
+/// ```rust
+/// use cyclonedx_bom::prelude::*;
+/// use test_utils::validate_json_with_schema;
+///
+/// let bom_json = r#"{
+///   "bomFormat": "CycloneDX",
+///   "specVersion": "1.3",
+///   "serialNumber": "urn:uuid:3e671687-395b-41f5-a30f-a58921a69b79",
+///   "version": 1
+/// }"#;
+/// let json: serde_json::Value = serde_json::from_str(bom_json).expect("Failed to parse JSON");
+/// let result = validate_json_with_schema(&json, SpecVersion::V1_3);
+/// assert!(result.is_ok());
+/// ```
+///
+pub fn validate_json_with_schema(
+    json: &serde_json::Value,
+    version: SpecVersion,
+) -> Result<(), Vec<ValidationError>> {
+    let spdx_schema = include_str!("../schema/spdx.schema.json");
+    let spdx_schema: serde_json::Value =
+        serde_json::from_str(spdx_schema).expect("Failed to read spdx.schema.json");
+    let jsf_schema = include_str!("../schema/jsf-0.82.schema.json");
+    let jsf_schema: serde_json::Value =
+        serde_json::from_str(jsf_schema).expect("Failed to load jsf-0.82.schema.json");
+
+    let schema = match version {
+        SpecVersion::V1_3 => include_str!("../schema/bom-1.3.schema.json"),
+        SpecVersion::V1_4 => include_str!("../schema/bom-1.4.schema.json"),
+        SpecVersion::V1_5 => include_str!("../schema/bom-1.5.schema.json"),
+    };
+    let schema: serde_json::Value =
+        serde_json::from_str(schema).expect("Failed to parse JSON schema file");
+
+    // Fill in external schema files, handle unknown format(s)
+    let compiled_schema = JSONSchema::options()
+        .with_draft(jsonschema::Draft::Draft7)
+        .with_document(
+            "http://cyclonedx.org/schema/spdx.schema.json".to_string(),
+            spdx_schema,
+        )
+        .with_document(
+            "http://cyclonedx.org/schema/jsf-0.82.schema.json".to_string(),
+            jsf_schema,
+        )
+        .with_format("idn-email", with_idn_email)
+        .compile(&schema)
+        .expect("Failed to compile JSON schema file");
+
+    let result = compiled_schema.validate(json);
+    if let Err(errors) = result {
+        let errors = errors.collect::<Vec<_>>();
+        dbg!(&errors);
+    }
+    compiled_schema.validate(json).map_err(|iter| {
+        iter.map(|err| ValidationError::new(err.instance.to_string(), err.kind, err.instance_path))
+            .collect::<Vec<_>>()
+    })
+}
+
+/// For now ignore the content of the given email string.
+fn with_idn_email(_s: &str) -> bool {
+    true
+}
+
+#[cfg(test)]
+mod test {
     use super::validate_json_with_schema;
     use cyclonedx_bom::models::bom::SpecVersion;
- 
-     #[test]
-     fn it_should_validate_version_13() {
-         let input = r#"
+
+    #[test]
+    fn it_should_validate_version_13() {
+        let input = r#"
  {
    "bomFormat": "CycloneDX",
    "specVersion": "1.3",
@@ -152,8 +152,7 @@
      }
    ]
  }"#;
-         let json = serde_json::from_str(input).expect("Failed to parse JSON");
-         assert!(validate_json_with_schema(&json, SpecVersion::V1_3).is_ok());
-     }
- }
- 
+        let json = serde_json::from_str(input).expect("Failed to parse JSON");
+        assert!(validate_json_with_schema(&json, SpecVersion::V1_3).is_ok());
+    }
+}
