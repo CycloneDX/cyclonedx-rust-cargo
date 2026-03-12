10d7dc97938442bac941d736a68be7060dc1f0fd
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-03-12 15:03:37 +0000

    Disable UI fail tests in cyclonedx-bom-macros temporarily
    Signed-off-by: Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>

--- cyclonedx-bom-macros/tests/lib.rs
+++ cyclonedx-bom-macros/tests/lib.rs
@@ -2,5 +2,12 @@
 fn ui() {
     let t = trybuild::TestCases::new();
     t.pass("tests/ui/pass/*.rs");
-    t.compile_fail("tests/ui/fail/*.rs");
+    // TODO: migrate away from `trybuild` crate and re-enable
+    // The `trybuild` crate is too limited for our purposes.
+    // We cannot use it to run tests on both MSRV and latest Rust version.
+    // Unlike e.g. `ui-test` it does not support different expected outputs per Rust version,
+    // and neither does it support checking that the build fails without matching the exact error message.
+    // And while it tries to compensate for some of the differences we cannot even use
+    // a recent version which does that because it won't compile on our MSRV!
+    // t.compile_fail("tests/ui/fail/*.rs");
 }
